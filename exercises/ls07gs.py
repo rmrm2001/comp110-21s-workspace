4a92a5f0315203a74bccd16012d0fca25e5db0d9
Meng Ren <mengren@ad.unc.edu>
2021-02-03 14:35:37 +0800

    ls07gs

---
+++ exercises/ls07gs.py
@@ -0,0 +1,5 @@
+
+a: int = 100
+b: int = a + 1
+c: bool = a == b - 1
+print (c)
--- lessons/LS07.py
+++ lessons/LS07.py
@@ -3,10 +3,14 @@
 how_much: int = int(input("Pick a number between 0-100."))
 
 if how_much < 50:
-print ("I Love You.")
+    print ("I Love You.")
 else:
-if how_much < 75:
-print ("I Really Love You.")
-else:
-print("ILYSM!!!")
+    if how_much < 75:
+        print("I Really Love You.")
+    else:
+        print("ILYSM!!!")
+
+if how_much == 50:
+    print("nah")
+"""this second if is a separate operation to the first if"""
 
