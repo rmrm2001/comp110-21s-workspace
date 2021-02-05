98ad4f6e172df36e6b3b845a8a7a41069e216209
Meng Ren <mengren@ad.unc.edu>
2021-02-05 13:43:57 +0800

    LS08

--- lessons/LS08.py
+++ lessons/LS08.py
@@ -6,4 +6,15 @@ def my_max (a: int, b:int) -> int:
         return a
     else:
         return b
-         
+
+print (my_max (10 + 1, 10))  
+
+x : int = 6
+y : int = 2 + 5
+print (my_max(x , y))
+
+"""same as below"""
+x : int = 6
+y : int = 2 + 5
+z : int = my_max (x , y)
+print (z)
