c1cd810e8f341b9b02cfa04efda38f69e6eac740
Meng Ren <mengren@ad.unc.edu>
2021-02-05 12:48:10 +0800

    LS08

--- exercises/ls07gs.py
+++ exercises/ls07gs.py
@@ -3,3 +3,9 @@ a: int = 100
 b: int = a + 1
 c: bool = a == b - 1
 print (c)
+
+a2: int = 2021
+b2: float = 3.14
+c2: str = "hello"
+d2: bool = a2 > 2020 and b2 < 4.0 or not c2 != "world"
+print(d2)
